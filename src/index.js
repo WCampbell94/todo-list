0b35604074a2a892573ce91a08b1cc7f1e1149de
William Campbell <campbellwilliam94@gmail.com>
2020-08-07 12:57:51 -0400

    Project initialized added README.MD, src(index.js, style.css), dist(index.html, main.js), and webpack config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function component() {
+	const element = document.createElement('div');
+
+	element.innerText = 'Hello world';
+
+	return element;
+}
+
+document.body.appendChild(component());
